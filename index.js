fbb1e1541e3da28e32e543d0d5d212b441772bda
Joan <joan.wafula@student.moringaschool.com>
2022-08-19 13:48:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1 = 31
+let num2 =2
+let multiply = num1*num2
+console.log (62)
+let random = 4
+Number.isInteger = true
+let max = (4,8,12,16,20)
+let num3 = 14
+let num4 = 5
+let mod = 4 
